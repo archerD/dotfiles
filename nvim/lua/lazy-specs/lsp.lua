fa59bae6dd600ad8ee78f46ac5a544ee7a6696ae
David Flores <dflores0818@gmail.com>
2023-12-22 12:23:01 -0700

    First pass at adding autocomplete to neovim.

--- nvim/lua/lazy-specs/lsp.lua
+++ nvim/lua/lazy-specs/lsp.lua
@@ -1,48 +1,60 @@
+local lsp_servers = function()
+    -- Define the language servers.
+    local lspconfig = require('lspconfig')
+
+    -- set the completion capabilities for all servers
+    local lsp_defaults = lspconfig.util.default_config
+    lsp_defaults.capabilities = vim.tbl_deep_extend(
+      'force',
+      lsp_defaults.capabilities,
+      require('cmp_nvim_lsp').default_capabilities()
+    )
+
+    lspconfig.hls.setup {}
+    lspconfig.rust_analyzer.setup {
+        settings = {
+            ['rust-analyzer'] = {},
+        },
+    }
+    lspconfig.nixd.setup {}
+    lspconfig.lua_ls.setup {    -- recommended setup for neovim lua from nvim-lspconfig
+        on_init = function(client)
+            local path = client.workspace_folders[1].name
+            if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+                client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+                    Lua = {
+                        runtime = {
+                            -- Tell the language server which version of Lua you're using
+                            -- (most likely LuaJIT in the case of Neovim)
+                            version = 'LuaJIT'
+                        },
+                        -- Make the server aware of Neovim runtime files
+                        workspace = {
+                            checkThirdParty = false,
+                            library = {
+                                vim.env.VIMRUNTIME
+                                -- "${3rd}/luv/library"
+                                -- "${3rd}/busted/library",
+                            }
+                            -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+                            -- library = vim.api.nvim_get_runtime_file("", true)
+                        }
+                    }
+                })
+
+                client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+            end
+            return true
+        end
+    }
+end
+
 return {
     -- language server and completion, based off builtin lsp support
     {"neovim/nvim-lspconfig",
+      dependencies = { 'hrsh7th/cmp-nvim-lsp' },
       config = function()
-        -- Define the language servers.
-        local lspconfig = require('lspconfig')
-        lspconfig.hls.setup {}
-        lspconfig.rust_analyzer.setup {
-          -- Server-specific settings. See `:help lspconfig-setup`
-          settings = {
-            ['rust-analyzer'] = {},
-          },
-        }
-        lspconfig.nixd.setup{}
-        lspconfig.lua_ls.setup{ -- recommended setup for neovim lua from nvim-lspconfig
-          on_init = function(client)
-            local path = client.workspace_folders[1].name
-            if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
-              client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-                Lua = {
-                  runtime = {
-                    -- Tell the language server which version of Lua you're using
-                    -- (most likely LuaJIT in the case of Neovim)
-                    version = 'LuaJIT'
-                  },
-                  -- Make the server aware of Neovim runtime files
-                  workspace = {
-                    checkThirdParty = false,
-                    library = {
-                      vim.env.VIMRUNTIME
-                      -- "${3rd}/luv/library"
-                      -- "${3rd}/busted/library",
-                    }
-                    -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                    -- library = vim.api.nvim_get_runtime_file("", true)
-                  }
-                }
-              })
-
-              client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-            end
-            return true
-          end
-        }
-
+        lsp_servers() -- define the lsp servers we use
         --- Setting up keybindings, based off https://github.com/neovim/nvim-lspconfig#suggested-configuration
         -- Global mappings.
         -- See `:help vim.diagnostic.*` for documentation on any of the below functions
@@ -62,7 +74,6 @@ return {
             -- Buffer local mappings.
             -- See `:help vim.lsp.*` for documentation on any of the below functions
             local opts = { buffer = ev.buf }
-            vim.keymap.set('i', '<C-<space>>', '<C-X> <C-O>', opts) -- remap the omnifunction to ctrl space
             vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
             vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
@@ -93,4 +104,72 @@ return {
       end,
     },
 
+    -- auto complete
+    { 'hrsh7th/nvim-cmp',
+      dependencies = {
+            'hrsh7th/cmp-nvim-lsp',
+            -- 'hrsh7th/cmp-vsnip', 'hrsh7th/vim-vsnip', -- vsnip snippets
+            'L3MON4D3/LuaSnip', -- luasnip snippets
+
+        },
+      config = function()
+        local cmp = require('cmp')
+        local luasnip = require("luasnip")
+
+        local has_words_before = function()
+            unpack = unpack or table.unpack
+            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+            return col ~= 0 and
+            vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+        end
+
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                  -- vim.fn["vsnip#anonymous"](args.body) -- vsnip snippets
+                  require('luasnip').lsp_expand(args.body) -- luasnip snippets
+                end,
+              },
+            sources = cmp.config.sources({
+                {name = 'nvim_lsp'},
+                {name = 'vsnip'}, -- vsnip snippets
+              }, {
+                { name = 'buffer' },
+              }),
+            mapping = cmp.mapping.preset.insert({
+                  ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                  ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                  ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+                  ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+                  ['<C-Space>'] = cmp.mapping.complete(), -- open the completion menu
+                  ['<C-e>'] = cmp.mapping.abort(),
+                  ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+                  -- from https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#super-tab-like-mapping
+                  ["<Tab>"] = cmp.mapping(function(fallback)
+                      if cmp.visible() then
+                          cmp.select_next_item()
+                          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+                          -- that way you will only jump inside the snippet region
+                      elseif luasnip.expand_or_jumpable() then
+                          luasnip.expand_or_jump()
+                      elseif has_words_before() then
+                          cmp.complete()
+                      else
+                          fallback()
+                      end
+                  end, { "i", "s" }),
+                  ["<S-Tab>"] = cmp.mapping(function(fallback)
+                      if cmp.visible() then
+                          cmp.select_prev_item()
+                      elseif luasnip.jumpable(-1) then
+                          luasnip.jump(-1)
+                      else
+                          fallback()
+                      end
+                  end, { "i", "s" }),
+                }),
+            })
+        end,
+    },
 }
