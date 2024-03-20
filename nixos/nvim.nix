abb0dc523c826ed2a70eaecd51031f11d11f68c0
David Flores <dflores0818@gmail.com>
2024-03-20 15:46:06 -0600

    Update nix flake lock file and reenable nixd.
    Now that nixd has been patched in nixos-23.11, the change disabling nixd
    can be reverted.

--- nixos/nvim.nix
+++ nixos/nvim.nix
@@ -59,7 +59,7 @@ in
         lspPkgs = [
           ghcPkg
           hlsPkg
-          # pkgs.nixd # TODO: uncomment line when nixd is in the nixos-23.11 channel.
+          pkgs.nixd
           # the cpp lsp is better served by having clang-tools in the flake.
           # pkgs.clang-tools
         ];
