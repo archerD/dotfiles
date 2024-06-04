c0632786c75435e0291c5e34f9f1bd0783be7cf6
David Flores <dflores0818@gmail.com>
2024-06-03 20:20:56 -0600

    Fiddle with home assistant configuration.
    Adds a note for potential future fiddling and fixes some errors.

--- nixos/home-assistant.nix
+++ nixos/home-assistant.nix
@@ -22,6 +22,8 @@
       "roku"
       "google_assistant"
       "ipp"
+      "tailscale"
+      "ibeacon" # to silence errors...
     ];
     extraPackages = py3ps: with py3ps; [
             pyqrcode # for 2fa
@@ -32,6 +34,10 @@
     config = {
       default_config = { };
       automation = "!include automations.yaml";
+      http = {
+        use_x_forwarded_for = true;
+        trusted_proxies = [ "127.0.0.1" ];
+      };
       homeassistant = {
         auth_mfa_modules = {
           type = "totp";
@@ -39,4 +45,6 @@
       };
     };
   };
+  # TODO: have a service to automatically run something like `sudo tailscale funnel --https=443 --set-path=/ "http://127.0.0.1:8123"`, or serve instead of funnel (serve is only accessible on the tailnet, funnel is publically availible).
+  # This makes home assistant available at the url nixos-desktop.tail80def.ts.net/
 }
