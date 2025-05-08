f40dc1095ed3e462b88bc94e89396981a0c91c5b
David Flores <dflores0818@gmail.com>
2025-05-07 19:50:59 -0600

    Stylix changes and notes on what stylix can do.

--- style.md
+++ style.md
@@ -14,13 +14,23 @@ carbonfox (with minor mods), vscode.nvim, vim-moonfly-colors, midnight.nvim, noc
 don't forget industry scheme...
 rodnaph/vim-color-schemes: nightshade, sean, rcg_term, dante, greens, Green, relaxedgreen, fog,
 
-# other notes:
-stylix documentation has suggestions for how to style things: [https://stylix.danth.me/styling.html]
-
 # rofi themes
 Themes I like for rofi (as given by rofi-theme-selector):
 fancy
 sidebar
 android_notification
 paper-float
+
+# Stylix stuff
+stylix documentation has suggestions for how to style things: [https://stylix.danth.me/styling.html]
+
+## colorschemes
+see the list above of general colorschemes
+## other things stylix can set
+in the future, it may be possible to use multiple wallpapers ...
+See if it is possible to combine the stylix theme with the rofi theme...
+fonts can also be set, for the font types serif, sansSerif, monospace, emoji; and sizes for usages.
+    - monospace font is set, others have not, sizes are also set, but may be adjusted...
+opacity can be set, with limited support.
+
 
--- stylix-theme.nix
+++ stylix-theme.nix
@@ -22,9 +22,42 @@
     terminal = 12;
     popups = 16;
   };
+
+  ### themes
   # stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/google-dark.yaml";
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/macintosh.yaml";
   # stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-latte.yaml";
   # stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/windows-10.yaml";
   # stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/primer-dark.yaml";
+
+  ### cursors and icons
+  stylix.cursor = {
+    # name = "gruppled_white_lite";
+    # package = pkgs.gruppled-white-lite-cursors;
+    # name = "volantes_cursors";
+    # package = pkgs.volantes-cursors;
+    # name = "Vimix-cursors";
+    # package = pkgs.vimix-cursors;
+    # name = "Hackneyed";
+    # package = pkgs.hackneyed;
+    name = "WhiteSur-cursors";
+    package = pkgs.whitesur-cursors;
+    size = 32;
+  };
+} // lib.optionalAttrs (!(lib ? nixosSystem))
+{
+  # HACK: this is only a home-manager option, consider moving into a hm module
+  stylix.iconTheme = lib.mkIf (builtins.hasAttr "archerd" config) {
+    enable = true;
+    package = pkgs.vimix-icon-theme;
+    dark = "Vimix-Doder-dark";
+    light = "Vimix-Doder";
+    # package = pkgs.papirus-icon-theme;
+    # dark = "Papirus-Dark";
+    # light = "Papirus-Light";
+    # package = pkgs.tela-icon-theme;
+    # dark = "Tela-green-dark";
+    # light = "Tela-green";
+  };
+
 }
