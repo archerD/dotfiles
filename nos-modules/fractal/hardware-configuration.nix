6cef04b2d74f0cb73feacfa032383a37bbaa4be1
David Flores <dflores0818@gmail.com>
2026-06-20 03:21:22 -0700

    Update fractal hardware and boot loader configuration.

--- nos-modules/fractal/hardware-configuration.nix
+++ nos-modules/fractal/hardware-configuration.nix
@@ -8,34 +8,23 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/8869812b-375f-4bd3-9d1e-fed66635b366";
+    { device = "/dev/disk/by-uuid/389e62f8-db70-4d56-bd53-7c95cbf452ea";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/BAA8-BBF7";
+    { device = "/dev/disk/by-uuid/9872-617F";
       fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
+      options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices =
-    [# { device = "/dev/disk/by-uuid/971163a1-aa5e-402a-af72-de2bb557f1a8"; }
-    ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp0s20f0u8.useDHCP = lib.mkDefault true;
+  swapDevices = [ ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
--- nos-modules/fractal/windows-drives.nix
+++ nos-modules/fractal/windows-drives.nix
@@ -8,7 +8,12 @@
   };
 
   fileSystems."/mnt/windows" = {
-    device = "/dev/disk/by-uuid/3AB02D53B02D1743";
+    device = "/dev/disk/by-uuid/ECC47676C476433E";
     fsType = "ntfs";
+  };
+
+  fileSystems."/mnt/nix-backup" = {
+    device = "/dev/disk/by-uuid/b4f918f2-4fd2-46c2-bcdb-d53240efab50";
+    fsType = "ext4";
   };
 }
